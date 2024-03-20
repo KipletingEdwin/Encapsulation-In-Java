47c258bc81afc249287b6232e9e43d1398822a55
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-03-20 21:06:25 +0300

    add getters and setters

---
+++ src/Employee.java
@@ -0,0 +1,37 @@
+public class Employee {
+    private String name;
+    private int ID;
+    private int salary;
+
+
+
+//    GETTERS AND SETTERS
+    public String getName(){
+        return name;
+    }
+
+    void setName(String name){
+        this.name = name;
+    }
+
+    public  int  getID(){
+        return  ID;
+    }
+
+    void setID(int ID){
+        this.ID = ID;
+    }
+
+    public  int getSalary(){
+        return  salary;
+    }
+
+    void setSalary(int salary){
+        this.salary = salary;
+    }
+
+
+
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,14 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Employee e = new Employee();
+        e.setName("Edwin");
+        e.setID(2);
+        e.setSalary(30000);
+
+        System.out.println("Name: " + e.getName());
+        System.out.println("ID: " + e.getID());
+        System.out.println("Salary: "+e.getSalary());
 
     }
 }
